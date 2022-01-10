2299194682b83874ae3244e97e9e4fbf4ca089f2
vikash kumar <vikashkumar620404800@gmail.com>
2022-01-10 05:55:45 +0530

    ch3completed

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import '../change_name_card.dart';
+import '../drawer.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController _nameController = TextEditingController();
+  var myText = "Change";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow[200],
+      appBar: AppBar(
+        title: Text("Flutter"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Card(
+            child:
+                ChangeNameCard(myText: myText, nameController: _nameController),
+          ),
+        ),
+      ),
+      drawer: MyDrawer(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          myText = _nameController.text;
+          setState(() {});
+        },
+        child: Icon(Icons.refresh),
+      ),
+    );
+  }
+}
